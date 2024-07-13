dfb4f025a9209115057956de6e164861c8026ca0
Tkctg <tkctgit@gmail.com>
2024-07-13 09:47:28 +0000

    index updated

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-app=Flask(__name__)
+import os
+from re import template
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+  return render_template('index.html')
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0',port=8080)
-  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
